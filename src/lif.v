9578a819b48bc0607532452eb701373be70e1494
a-lawn-flamingo <ywan1115@ucsc.edu>
2026-02-27 16:53:35 -0800

    fixed?

--- src/lif.v
+++ src/lif.v
@@ -1,27 +1,26 @@
-`default_nettype 
+`default_nettype none
 
 module lif (
-    input wire [7:0]    current,
-    input wire          clk,
-    input wire          rst_n,
-    output wire [7:0]   state,
-    output wire         spike,
-
+    input wire [7:0]  current,
+    input wire        clk,
+    input wire        rst_n,
+    output reg [7:0]  state,
+    output wire       spike
 );
-    wire[7:0] next_state;
-    reg[7:0] threadhold;
 
-    always @(posedge clk) begin
+    wire [7:0] next_state;
+    reg  [7:0] threshold;
+
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            state <= 0;
-            threadhold <= 200;
+            state <= 8'd0;
+            threshold <= 8'd200;
         end else begin
             state <= next_state;
         end
-        
     end
 
     assign next_state = current + (state >> 1);
-    assign spike = (state >= threadhold);
-    
+    assign spike = (state >= threshold);
+
 endmodule
